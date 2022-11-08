7bd6165eeae37ea593cb4694a553fc8e813730b0
Charlotte Qazi <charlottemdavies@hotmail.co.uk>
2022-11-08 17:57:31 +0800

    Refactored Activity

--- app/src/main/java/com/example/whereswally/views/LocationScreen.kt
+++ app/src/main/java/com/example/whereswally/views/LocationScreen.kt
@@ -1,8 +1,5 @@
 package com.example.whereswally.views
 
-import android.content.pm.PackageManager
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Switch
@@ -10,32 +7,22 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import com.example.whereswally.viewmodels.*
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
 import com.google.accompanist.permissions.rememberPermissionState
-import java.util.jar.Manifest
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun LocationScreen(
     locationViewModel: ILocationViewModel
 ) {
-    var latInput = locationViewModel.locationFromGps?.lat ?: 0.0
-    var longInput = locationViewModel.locationFromGps?.long ?: 0.0
-    var speedInput = locationViewModel.locationFromGps?.speed ?: 0.0F
-
-    var trackingOnOffState by remember { mutableStateOf(false) }
-
-    var trackingOn = turnTrackingOnOrOff(trackingOnOffState, locationViewModel)
 
     val fineLocationPermission = rememberPermissionState(android.Manifest.permission.ACCESS_FINE_LOCATION)
-
+    val permissionStatus = fineLocationPermission.status
 
     Column(
         modifier = Modifier.padding(32.dp),
@@ -48,6 +35,48 @@ fun LocationScreen(
         )
         Spacer(Modifier.height(16.dp))
 
+        when (fineLocationPermission.status) {
+            PermissionStatus.Granted -> {
+                Button(
+                    onClick = {
+                        locationViewModel.startTracking()
+                    },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ){
+                    Text(text = "Start Tracking")
+                }
+                DisplayLocationData(locationViewModel)
+            }
+            else -> {
+                Text(text = "You must give permission to use this app.")
+                Button(
+                    onClick = {fineLocationPermission.launchPermissionRequest()}
+                ){
+                    Text(text = "Give permission")
+                }
+            }
+        }
+
+    }
+
+
+}
+
+@Composable
+fun DisplayLocationData(
+    locationViewModel: ILocationViewModel
+) {
+
+    var latInput = locationViewModel.locationFromGps?.lat ?: 0.0
+    var longInput = locationViewModel.locationFromGps?.long ?: 0.0
+    var speedInput = locationViewModel.locationFromGps?.speed ?: 0.0F
+
+    var trackingOnOffState by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
         Text(text = "Lat:  " + latInput)
         Text(text = "Lon:  " + longInput)
         Text("Speed: " + speedInput)
@@ -60,34 +89,6 @@ fun LocationScreen(
                 onCheckedChange = { trackingOnOffState = it }
             )
         }
-
-        Button(onClick = { locationViewModel.startTracking() }) {
-            Text(text = "Start Tracking ")
-        }
-
-        Button(
-            onClick = {
-                when (fineLocationPermission.status) {
-                    PermissionStatus.Granted -> {
-                        locationViewModel.startTracking()
-                    }
-                    else -> {
-                        fineLocationPermission.launchPermissionRequest()
-                    }
-                }
-            }
-        ){
-            Text(text = "Check and Request Permission")
-        }
-    }
-}
-
-fun turnTrackingOnOrOff(
-    trackingOnOffState: Boolean,
-    viewModel: ILocationViewModel
-) {
-    if (trackingOnOffState) {
-        viewModel.startTracking()
     }
 }
 
